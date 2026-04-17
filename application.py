3fe697963cfba6b48dd94354239d638c177d5c97
AryanRajendraDalvi <145216558+AryanRajendraDalvi@users.noreply.github.com>
2026-04-18 00:34:59 +0530

    Update application.py

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ application = Flask(__name__)
 
 def hello_world(): 
 
-return "<h1>CI/CD Pipeline is Live!</h1><p>Version 1.0: Initial Deployment.</p>" 
+  return "<h1>CI/CD Pipeline is Live!</h1><p>Version 1.0: Initial Deployment.</p>" 
 
 if __name__ == '__main__': 
 
